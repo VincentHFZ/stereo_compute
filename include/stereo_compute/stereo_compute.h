01d5c6c68690d9ec400e1168a2f66e68698fc351
HuangFazhan <534050016@qq.com>
2018-08-27 11:48:06 +0800

    nothing

--- include/stereo_compute/stereo_compute.h
+++ include/stereo_compute/stereo_compute.h
@@ -1,6 +1,21 @@
 #ifndef _STEREO_COMPUTE_H_
 #define _STEREO_COMPUTE_H_
 
+// std libs
+#include <iostream>
+#include <string>
+#include <vector>
+// ros
+#include <ros/ros.h>
+#include <sensor_msgs/PointCloud.h>
+#include <sensor_msgs/Image.h>
+#include <image_transport/image_transport.h>
+// opencv
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+
 
 
 #endif
